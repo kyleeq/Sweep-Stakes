bc58072a87f365530b7fb76fbe68e7d54474b293
unknown <kylee.schreibs2@yahoo.com>
2019-04-17 10:21:52 -0500

    add GetInfo method and functionality back into Contestant class and add GetRegistrationNumber method that grabs a random number

--- SweepStakes/Contestant.cs
+++ SweepStakes/Contestant.cs
@@ -9,13 +9,33 @@ namespace SweepStakes
     public class Contestant
     {
         // member variables
-        string firstName;
-        string lastName;
-        string emailAddress;
-        int registrationNumber;
+        
+        public string firstName;
+        public string lastName;
+        public string emailAddress;
+        public int registrationNumber;
+
+        Random rand;
 
         // constructor
+        public Contestant()
+        {
+            rand = new Random();
+        }
 
         // member methods
+        public void GetInfo()
+        {
+            firstName = UserInterface.FirstNamePrompt();
+            lastName = UserInterface.LastNamePrompt();
+            emailAddress = UserInterface.EmailAddress();
+        }
+
+        public int GetRegistrationNumber(int min, int max)
+        {
+            registrationNumber = rand.Next(1000, 10000);
+            Console.WriteLine($"You're registration number is {registrationNumber}. Make sure to write it down!");
+            return registrationNumber;
+        }
     }
 }
---
+++ SweepStakes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStakes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate marketing firm, contestants and sweepstakes
+            // marketting does functionality upon injection
+            // add sweepsstakes and then contestants
+        }
+    }
+}
