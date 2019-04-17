1b4fb42e7c28c0ad0eaa8b4996696c72a0fd5187
unknown <kylee.schreibs2@yahoo.com>
2019-04-16 21:06:28 -0500

    add ISweepstakesManager interface and member methods

---
+++ SweepStakes/ISweepstakesManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStakes
+{
+    interface ISweepstakesManager
+    {
+        // member methods
+        void InsertSweepstakes(Sweepstakes sweepstakes);
+
+        Sweepstakes GetSweepstakes();
+        
+    }
+}
